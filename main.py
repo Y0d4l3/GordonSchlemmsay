30c52a171de391b2bcf73186f8552a1d6d18e0a4
Michel <yodale1000@live.de>
2023-09-21 20:16:00 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import argparse
+from typing import List
+
+import requests
+from bs4 import BeautifulSoup
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+
+class Dish:
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
+
+
+def fetch_dishes() -> List[Dish]:
+    url = f"https://www.kstw.de/speiseplan?l=22"
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, 'html.parser')
+    dishes = []
+    menu = soup.find("div", {"class": "tx-epwerkmenu-menu-locationpart-wrapper"})
+    for menu_tile in menu.find_all("div", {"class": "menue-tile"}):
+        dishes.append(Dish(
+            name=menu_tile.find("div", {"class": "tx-epwerkmenu-menu-meal-title"}).get_text(strip=True),
+            description=menu_tile.find("div", {"class": "tx-epwerkmenu-menu-meal-description"}).get_text(strip=True),
+        ))
+    return list(dishes)
+
+
+def send_webhook(webhook_url):
+    dishes = fetch_dishes()
+    if not dishes:
+        return
+    kinds = ["VEGAN", "VEGETARISCH"]
+    embeds = [DiscordEmbed(title=dish.name, description=dish.description,
+                           color="429E12" if any([x in dish.name for x in kinds]) else "C92423") for dish in dishes]
+    webhook = DiscordWebhook(url=webhook_url)
+    for embed in embeds:
+        webhook.add_embed(embed)
+    res = webhook.execute()
+    res.raise_for_status()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Gordon Schlemmsay Canteen Bot")
+    parser.add_argument("--webhook", type=str, default="", help="Sets the webhook path")
+    args = parser.parse_args()
+    send_webhook(args.webhook)
