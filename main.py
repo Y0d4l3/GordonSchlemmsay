50d96e617397595a5269df723af67a651ca95eb9
Michel <yodale1000@live.de>
2023-09-27 12:13:16 +0200

    update

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 
 
 class Dish:
-    def __init__(self, name: str, description: str):
+    def __init__(self, name: str, description: str, image_url: str):
         self.name = name
         self.description = description
+        self.image_url = image_url
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __hash__(self):
+        return hash(self.name)
 
 
 def fetch_dishes() -> List[Dish]:
@@ -20,11 +27,13 @@ def fetch_dishes() -> List[Dish]:
     dishes = []
     location = soup.find("div", {"class": "tx-epwerkmenu-menu-location-wrapper", "data-location": 22})
     for menu_tile in location.find_all("div", {"class": "col-12 col-lg-6 mb-4 menue-tile"}):
+        plate = menu_tile.find("div", {"class": "plate"})
         dishes.append(Dish(
             name=menu_tile.find("div", {"class": "tx-epwerkmenu-menu-meal-title"}).get_text(strip=True),
             description=menu_tile.find("div", {"class": "tx-epwerkmenu-menu-meal-description"}).get_text(strip=True),
+            image_url=plate.find("img")["src"]
         ))
-    return dishes
+    return list(set(dishes))
 
 
 def send_webhook(webhook_url):
@@ -32,8 +41,12 @@ def send_webhook(webhook_url):
     if not dishes:
         return
     kinds = ["VEGAN", "VEGETARISCH"]
-    embeds = [DiscordEmbed(title=dish.name, description=dish.description,
-                           color="429E12" if any([x in dish.name for x in kinds]) else "C92423") for dish in dishes]
+    embeds = []
+    for dish in dishes:
+        color = "429E12" if any([x in dish.name for x in kinds]) else "C92423"
+        embed = DiscordEmbed(title=dish.name, description=dish.description, color=color)
+        embed.set_image(dish.image_url)
+        embeds.append(embed)
     webhook = DiscordWebhook(url=webhook_url)
     for embed in embeds:
         webhook.add_embed(embed)
