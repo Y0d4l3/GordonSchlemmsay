c484f43b40248ea2d71305254edf512d31528e79
Michel Haurand <41866123+Yodale1000@users.noreply.github.com>
2023-10-17 12:34:02 +0200

    update

--- cronjob.py
+++
@@ -1,8 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-import os
-from main import send_webhook
-
-scheduler = BlockingScheduler()
-schedule.every().day.at("09:00").do(send_webhook, os.environ['WEBHOOK-URL'])
-
-scheduler.start()
